8413617f1c5e81ce4661d982fb6d749678cc580a
Kleber de Oliveira Andrade <pdjkleber@gmail.com>
2015-04-16 23:44:28 -0300

    Projeto criado durante o minicurso. Na época foi usado na Unity 4.6, então vários problemas podem acontecer em versões superiores

---
+++ Assets/Scripts/CarControl.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarControl : MonoBehaviour
+{
+    public Transform centerMass;
+
+    public WheelCollider wheelL;
+    public WheelCollider wheelR;
+
+    public Transform objWheelL;
+    public Transform objWheelR;
+
+    public float maxTorque;
+    public float brake;
+    public float friction;
+
+    private float torqueL;
+    private float torqueR;
+
+    void Start()
+    {
+        //altera centro de massa
+        rigidbody.centerOfMass = centerMass.localPosition; 
+    }
+
+    void FixedUpdate()
+    {
+        // armazena input do jogador
+        torqueL = maxTorque * Input.GetAxis("Vertical");
+        torqueR = maxTorque * Input.GetAxis("Vertical2");
+        
+        // aplica rotação das rodas de acordo com a velocidade do carro
+        objWheelL.Rotate(0, 0, wheelL.rpm / 60 * 360 * Time.deltaTime);
+        objWheelR.Rotate(0, 0, wheelR.rpm / 60 * 360 * Time.deltaTime);  
+
+        // aplica o torque nas rodas
+        wheelL.motorTorque = maxTorque * torqueL;
+        wheelR.motorTorque = maxTorque * torqueR;
+
+
+        // desacelera naturalmente
+        if (torqueL == 0.0f && torqueR == 0.0f)
+        {
+            wheelL.motorTorque = 0;
+            wheelR.motorTorque = 0;            
+
+            wheelL.brakeTorque = friction;
+            wheelR.brakeTorque = friction;
+            print("desacelerando");
+        }
+
+        // frea
+        if (Input.GetKey(KeyCode.Space))
+        {
+            wheelL.brakeTorque = brake;
+            wheelR.brakeTorque = brake;
+            print("freando");
+        }
+    }
+}
---
+++ Assets/Scripts/WheelAlignment.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelAlignment : MonoBehaviour
+{
+
+    public WheelCollider wheelCollider;
+
+    private RaycastHit hit;
+
+
+    void FixedUpdate()
+    {
+        //posiçao do centro do wheelcollider 
+        Vector3 centerCollider = wheelCollider.transform.position;
+
+        //lança um raio a partir do centor do wcollider, para baixo, do tamanho do raio da roda mais a suspensao
+        if (Physics.Raycast(centerCollider, -wheelCollider.transform.up, out hit, wheelCollider.suspensionDistance + wheelCollider.radius))
+            //se tocar em algo comprime a roda para cima a partir do ponto de contato
+            transform.position = hit.point + (wheelCollider.transform.up * wheelCollider.radius);
+        else
+            //senaoa suspensao extende a roda para baixo a partir do seu centro
+            transform.position = centerCollider - (wheelCollider.transform.up * wheelCollider.suspensionDistance);
+    }
+}
